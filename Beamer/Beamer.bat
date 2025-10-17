47daeb066f1aefd81f43b2d84f7221111c161e8f
Arnaud Natal <arnaud.natal@ifpindia.org>
2025-10-17 12:26:14 +0200

    Delete Beamer directory

--- Beamer/Beamer.bat
+++
@@ -1,3 +0,0 @@
-arara %~n0.tex
-
-pandoc -s %~n0.tex --filter pandoc-crossref --citeproc --csl="https://raw.githubusercontent.com/citation-style-language/styles/master/apa-6th-edition.csl" --reference-doc reference.docx -o %~n0.docx
